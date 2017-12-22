9de24f90ae34597111b27f74f014439e85a85afe
morpheous <cerbus.collin@gmail.com>
2017-12-22 04:35:24 -0800

    8

--- SCANNERS/wiley.js
+++ SCANNERS/wiley.js
@@ -34,7 +34,7 @@ function CUSTOM_RESULT_PAGE_PARSER( wBody ) {
 					doi: wDOI ,
 					doiB64: EncodeB64( wDOI ) ,
 					mainURL: WILEY_ARTICLE_BASE_URL + wDOI + "/abstract" ,
-					scihubURL: SCI_HUB_BASE_URL + wDOI ,
+					scihubURL: WILEY_ARTICLE_BASE_URL + wDOI + "/epdf" ,
 				});
 			});
 			resolve( finalResults );
