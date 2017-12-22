7605d63cd67c98e71015a3248f82d418bc0c1dfd
morpheous <cerbus.collin@gmail.com>
2017-12-22 04:37:25 -0800

    9

--- main.js
+++ main.js
@@ -87,38 +87,10 @@ function SCAN_HOURLIES() {
 function SCAN_EVERYTHING() {
 	return new Promise( async function( resolve , reject ) {
 		try {
-			try{ await require( "./SCANNERS/plos.js" ).search(); }
-			catch( e ) { console.log( e ); }
-			try{ await require( "./SCANNERS/cell.js" ).search( "month" ); }
-			catch( e ) { console.log( e ); }
-			try{ await require( "./SCANNERS/scienceDirect.js" ).search(); }
-			catch( e ) { console.log( e ); }
-			try{ await require( "./SCANNERS/jmir.js" ).search(); }
-			catch( e ) { console.log( e ); }
-			try{ await require( "./SCANNERS/pubmed.js" ).search( [ "autism" , "autistic" ] ); }
-			catch( e ) { console.log( e ); }
-			try{ await require( "./SCANNERS/subreddit.js" ).search( [ "science" , "new" , [ "autis" ] ] ); }
-			catch( e ) { console.log( e ); }
-			try{ await require( "./SCANNERS/subreddit.js" ).search( [ "science" , "top" , [ "autis" ] ] ); }
-			catch( e ) { console.log( e ); }
-			try{ await require( "./SCANNERS/nature.js" ).search(); }
-			catch( e ) { console.log( e ); }
-			try{ await require( "./SCANNERS/mdpi.js" ).search(); }
-			catch( e ) { console.log( e ); }
-			try{ await require( "./SCANNERS/wiley.js" ).search(); }
-			catch( e ) { console.log( e ); }
-			try{ await require( "./SCANNERS/oup.js" ).search(); }
-			catch( e ) { console.log( e ); }
-			try{ await require( "./SCANNERS/spectrumNews.js" ).search(); }
-			catch( e ) { console.log( e ); }
-			try{ await require( "./SCANNERS/springer.js" ).search(); }
-			catch( e ) { console.log( e ); }
-			try{ await require( "./SCANNERS/karger.js" ).search(); }
-			catch( e ) { console.log( e ); }
-			try{ await require( "./SCANNERS/frontiersin.js" ).search(); }
-			catch( e ) { console.log( e ); }
-			try{ await require( "./SCANNERS/biorxiv.js" ).search(); }
-			catch( e ) { console.log( e ); }
+			await SCAN_DAYLIES();
+			await SCAN_TWICE_DAYLIES();
+			await SCAN_THREE_HOUR();
+			await SCAN_HOURLIES();
 			resolve();
 		}
 		catch( error ) { console.log( error ); reject( error ); }
